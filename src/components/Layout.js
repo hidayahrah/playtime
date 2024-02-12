c4acfde97a7785d0ce7d2e3218db1cc387893e20
hidayahrah <hidayahrah97@gmail.com>
2024-02-13 02:50:32 +0800

    Added footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import { Inconsolata } from "next/font/google";
+import LoveSVG from "./icons/Love";
 
 export const font = Inconsolata({
   subsets: ["latin"],
@@ -6,11 +7,22 @@ export const font = Inconsolata({
 });
 
 export default function Layout({ children }) {
+  const currYear = new Date().getFullYear();
   return (
-    <div
-      className={`relative flex items-center justify-center ${font.className}`}
-    >
-      <main className="overflow-auto h-screen w-full">{children}</main>
+    <div className={`${font.className}`}>
+      <main className="relative overflow-auto h-screen w-full">
+        {children}
+        <footer className="m-8 md:my-10 md:mx-24 flex flex-col md:flex-row items-center justify-between">
+          <div className="flex flex-row gap-2 items-center justify-center">
+            Made with{" "}
+            <span className="animate-bounce w-4 h-4">
+              <LoveSVG />
+            </span>
+            by hidayahrah. © {currYear} All rights reserved.
+          </div>
+          <a href="https://github.com/hidayahrah">GitHub</a>
+        </footer>
+      </main>
     </div>
   );
 }
---
+++ src/components/icons/Love.js
@@ -0,0 +1,13 @@
+export default function LoveSVG() {
+  return (
+    <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m7 12.45l-5.52-5c-3-3 1.41-8.76 5.52-4.1c4.11-4.66 8.5 1.12 5.52 4.1Z"
+      />
+    </svg>
+  );
+}
